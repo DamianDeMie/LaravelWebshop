1c3cffada20f5df56cf0f06dee607159709bfaf7
Damian <42833051+DamianDeMie@users.noreply.github.com>
2020-06-01 13:17:58 +0200

    Started work on Product Seeder

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -11,10 +11,11 @@ class ProductsSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10) . '@gmail.com',
-            'password' => Hash::make('password'),
+        DB::table('products')->insert([
+            'product_name' => 'Epiphone Les Paul Standard',
+            'product_price' => 399.99,
+            'description' => 'Sinds de introductie in 1952 is de Les Paul niet meer weg te denken uit gitaarland. Jimmy Page, Slash, Joe Perry, Zakk Wylde, Lenny Kravitz en vele anderen spelen al decennia lang op Les Pauls. De klank is dan ook legendarisch. Met de Epiphone Les Paul Standard kan iedereen een echte Les Paul in handen krijgen voor een relatief vriendelijke prijs. Epiphone is het dochtermerk van Gibson, dus echtheid en kwaliteit zijn absoluut gegarandeerd.',
+            'image' => 'images/products/epiphone-les-paul-standard-60s-ebony.jpg',
         ]);
     }
 }
