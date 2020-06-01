f79a90a2880c3f2f81804536b713d968b4d788c7
Damian <42833051+DamianDeMie@users.noreply.github.com>
2020-06-01 12:49:02 +0200

    Added Categories Seeder

---
+++ database/seeds/CategoriesSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            'name' => 'Gitaren',
+            'image' => 'images/categories/guitar.png'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Basgitaren',
+            'image' => 'images/categories/bass.png'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Microfoons',
+            'image' => 'images/categories/mic.jpg'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Drums',
+            'image' => 'images/categories/drumkit.jpg'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Toetsinstrumenten',
+            'image' => 'images/categories/ta.jpg'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Software',
+            'image' => 'images/categories/sw.jpg'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Studio-toebehoren',
+            'image' => 'images/categories/st.jpg'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Accessories',
+            'image' => 'images/categories/ca.jpg'
+        ]);
+    }
+}
