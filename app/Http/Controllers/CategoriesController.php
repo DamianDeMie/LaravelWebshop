03c8aa6f54f747461871e2abd3a959fe67e98aea
Damian <42833051+DamianDeMie@users.noreply.github.com>
2020-06-10 13:22:39 +0200

    Fixed bug

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -13,7 +13,6 @@ class CategoriesController extends Controller
 {
     public function getProducts($id)
     {
-        $products = DB::table('products')->get()->where('category_id', $id);
         $products = Products::get()->where('category_id', $id);
         $categories = Categories::all();
         return view('categories.index', ['products' => $products, 'categories' => $categories]);
